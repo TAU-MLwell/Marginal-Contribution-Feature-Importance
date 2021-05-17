37d619b6e9742de780ceb311d506ac9fd8327d70
Amnon Catav <amnoncatav@microsoft.com>
2021-05-17 11:37:11 +0300

    package name change

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='MCI',
-    version='0.1.1',
+    name='MLWELL-MCI',
+    version='0.1.0',
     packages=find_packages(),
     description='implementation of the marginal contribution feature importance score',
     install_requires=[
