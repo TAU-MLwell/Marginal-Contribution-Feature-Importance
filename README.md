635f24c09e2fdf06941d35946bcc06ded6246009
Amnon Catav <amnoncatav@microsoft.com>
2021-05-22 12:22:55 +0300

    add config files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MCI
-source code for Marginal Contribution Importance (MCI) method
+# Marginal-Contribution-Feature-Importance
+source code for Marginal Contribution Importance (MCI) method published in ICML 2021.
+Usage example can be found under notebooks folder.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='MLWELL-MCI',
+    name='Marginal-Contribution-Feature-Importance',
     version='0.1.0',
     packages=find_packages(),
     description='implementation of the marginal contribution feature importance score',
