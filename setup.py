38deb842ebcd074bdf4cb9161976640c5e283ba7
Amnon Catav <amnoncatav@microsoft.com>
2021-05-22 12:26:33 +0300

    add release to setup file

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     description='implementation of the marginal contribution feature importance score',
+    download_url='https://github.com/TAU-MLwell/Marginal-Contribution-Feature-Importance/archive/refs/tags/0.1.0.tar.gz',
     install_requires=[
         'numpy',
         'matplotlib',
